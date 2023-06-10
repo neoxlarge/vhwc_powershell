984003c3f47de302d88fe9fb7a43a689186d967b
vhwc73058 <vhwc73058@outlook.com>
2023-06-10 20:22:38 +0800

    test git on vhwc73058

--- test.ps1
+++ test.ps1
@@ -1,22 +1,3 @@
 Function Prevent-SleepWmi {
-    $Info = Get-CimInstance -ClassName CIM_SessionStatistics
-    if (($Info | Select-Object -ExpandProperty IsActive) -contains $true) {
-        Return;
-    }
-
-    $wmiParams = @{ 
-        Namespace = 'root\cimv2\power' 
-        Class = 'Win32_PowerSettingDataIndex'
-    }
-    $sleepSettings = Get-CimInstance @wmiParams -Filter "InstanceID='Microsoft:PowerSetting\{155AAB8B-23B9-4A29-87F0-CPF60A52EEFF}\SUB_NONE_7516b95f-008f-4cb4-9116-DC2EF728B5B6'"
-    $displaySettings = Get-CimInstance @wmiParams -Filter "InstanceID='Microsoft:PowerSetting\{3C0BC021-C8A8-4E07-A973-6B14CBCB2B7E}\SUB_NONE_ultimate_power_plan_ac_dll_microsoft_powerplan_6d22571a_03e8_4eed_80b8_6bdd79498fc8'"
-    Set-CimInstance -InputObject $sleepSettings[0] -Arguments @{ 
-        AcValueIndex = 0
-        DcValueIndex = 0
-    }
-    Set-CimInstance -InputObject $displaySettings[0] -Arguments @{$_.PropertyName = $_.PropertyValue for $_ in $displaySettings.Properties}
+   
 }
-
-
-Set-ItemProperty HKCU:\Software\Classes\Local Settings\Software\Microsoft\Windows\Shell\Associations\UrlAssociations\http\UserChoice ProgId "IE.HTTP"
-Restart-Explorer
