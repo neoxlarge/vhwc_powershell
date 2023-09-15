f1c5e88a075c5f45ee6d7f555e49e95381e9e6e5
neoxlarge_aio <neoxlarge@gmail.com>
2023-09-16 00:14:08 +0800

    use compare-versions

--- 救救讀卡機計時1小時.ps1
+++ 救救讀卡機計時1小時.ps1
@@ -8,6 +8,37 @@
 
 param($runadmin)
 
+function Compare-Versions {
+  param (
+    [Parameter(Mandatory = $true)]
+    [string]$Version1, # 第一?版本
+
+    [Parameter(Mandatory = $true)]
+    [string]$Version2     # 第二?版本
+  )
+
+  # 將版本號拆分成陣列，以便逐個比較各個部分
+  $version1Array = $Version1.Split('.')
+  $version2Array = $Version2.Split('.')
+
+  # 使用 foreach 迴圈遍歷每個部分進行比較
+  foreach ($i in 0..$version1Array.Count) {
+    if ([int]$version1Array[$i] -gt [int]$version2Array[$i]) {
+      return $true    # 返回 $true 表示第一個版本號大於第二個版本號
+    }
+    elseif ([int]$version1Array[$i] -lt [int]$version2Array[$i]) {
+      return $false   # 返回 $false 表示第一個版本號小於第二個版本號
+    }
+    else {
+      # 如果當前部分相等，則繼續比較下一個部分
+      continue
+    }
+  }
+
+  # 如果完全相同，則表示版本號相同
+  return $true    # 返回 $true 表示兩個版本號相同
+}
+
 
 function remove-HiddenDevice {
 
@@ -28,7 +59,7 @@ function remove-HiddenDevice {
   )
   foreach ($p in $pnputil_files) {
     $p_version = Get-ItemPropertyValue -Path $p -Name VersionInfo
-    if ($p_version.FileVersion -ge [version]"10.0.19041.3323") {
+    if ((Compare-Versions -Version1 $p_version.ProductVersion -Version2 "10.0.19041.3155")) {
       $pnputil_path = $p
       break
     }
@@ -42,7 +73,7 @@ function remove-HiddenDevice {
 
     foreach ($p in $pnputil_files) {
       $p_version = Get-ItemPropertyValue -Path $p -Name VersionInfo
-      if ($p_version.FileVersion -ge [version]"10.0.19041.3323") {
+      if ((Compare-Versions -Version1 $p_version.ProductVersion -Version2 "10.0.19041.3155")) {
         Copy-Item -Path $p -Destination $env:TEMP -Force -Verbose
         $pnputil_path = "$($env:temp)\pnputil.exe"
         break
