f2f9bc5380396606513e1bdd9d6899abebdd095d
vhwc73058 <vhwc73058@outlook.com>
2023-12-05 10:31:29 +0800

    add AST auto , and delete old file.

---
+++ install-NHICardReaderOCX.ps1
@@ -0,0 +1,77 @@
+# 安裝工具程式7z
+
+param($runadmin)
+
+Import-Module ((Split-Path $PSCommandPath) + "\get-installedprogramlist.psm1")
+
+function install-7z {
+    
+    $software_name = "7-Zip*"
+    $software_path = "\\172.20.5.187\mis\07-7-7z"
+    $software_msi = "7z2201-x64.msi"
+    $software_msi_x86 = "7z2201-x32.msi"
+
+    ## 找出軟體是否己安裝
+
+    $all_installed_program = get-installedprogramlist
+    $software_is_installed = $all_installed_program | Where-Object -FilterScript { $_.DisplayName -like $software_name }
+
+    if ($null -eq $software_is_installed) {
+    
+        Write-Output "Start to insall: $software_name"
+
+        #復制檔案到本機暫存"
+        $software_path = get-item -Path $software_path
+        Copy-Item -Path $software_path -Destination $env:temp -Recurse -Force -Verbose
+
+        ## 判斷OS是32(x86)或是64(AMD64), 其他值(ARM64)不安裝  
+        switch ($env:PROCESSOR_ARCHITECTURE) {
+            "AMD64" { $software_exec = $software_msi }
+            "x86" { $software_exec = $software_msi_x86 }
+            default { Write-Warning "Unsupport CPU or OS:"  $env:PROCESSOR_ARCHITECTURE; $software_exec = $null }
+        }
+
+        if ($null -ne $software_exec) {
+            Start-Process -FilePath "msiexec.exe" -ArgumentList "/i $($env:temp + "\" + $software_path.Name + "\" + $software_exec) /passive " -Wait
+            Start-Sleep -Seconds 5 
+        }
+        else {
+            Write-Warning "$software_name 無法正常安裝."
+        }
+      
+        
+        #安裝完, 再重新取得安裝資訊
+        $all_installed_program = get-installedprogramlist
+        $software_is_installed = $all_installed_program | Where-Object -FilterScript { $_.DisplayName -like $software_name }
+    
+    }
+
+    Write-Output ("Software has installed: " + $software_is_installed.DisplayName)
+    Write-Output ("Version: " + $software_is_installed.DisplayVersion)
+
+}
+
+
+
+
+
+#檔案獨立執行時會執行函式, 如果是被載入時不會執行函式.
+if ($run_main -eq $null) {
+
+    #檢查是否管理員
+    $check_admin = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
+
+    if (!$check_admin -and !$runadmin) {
+        #如果非管理員, 就試著run as admin, 並傳入runadmin 參數1. 因為在網域一般使用者永遠拿不是管理員權限, 會造成無限重跑. 此參數用來輔助判斷只跑一次. 
+        Start-Process powershell.exe -ArgumentList "-FILE `"$PSCommandPath`" -Executionpolicy bypass -NoProfile  -runadmin 1" -Verb Runas; exit
+    
+    }
+
+    if ($check_admin) { 
+        install-7z    
+    }
+    else {
+        Write-Warning "無法取得管理員權限來安裝軟體, 請以管理員帳號重試."
+    }
+    pause
+}
