15f2915239dfdf75d7d5f9d65bafb04e17ae6623
neoxlarge_aio <neoxlarge@gmail.com>
2024-02-14 20:33:22 +0800

    check-systembackup

--- check-systemback.ps1
+++ check-systemback.ps1
@@ -82,4 +82,40 @@ if ($latest_file) {
 
 Send-LineNotifyMessage -Message $line_msg
 
+#取得星期縮寫
+$today_ofweek = (get-date).DayOfWeek.ToString().Substring(0,3)
+
 # \\172.20.1.122\backup\001-014-dbSTUDY\dbSTUDY_Mon.zip
+# 1.檔案存在
+# 2.檔名結構正確
+# 3.檔案日期正確
+# 3.檔案大小正常
+
+$path = "\\172.20.1.122\backup\001-014-dbSTUDY"
+$zip_filename = "dbSTUDY_$today_ofweek.zip"
+
+$check_filepath = Test-Path -Path "$path\$zip_filename"
+
+
+if ($check_filepath) {
+    #檔案存在,檢查檔案日期
+    $latest_file = get-item -Path "$path\$zip_filename"
+    $check_dateoffile = $latest_file.LastWriteTime.ToString("yyyyMMdd") -eq $today
+
+    if ($check_dateoffile) {
+        #日期正確,檢查檔案大小
+        $check_filesize =  $latest_file.Length -gt 1500kb -and $latest_file.Length -lt 2000kb
+
+        if ($check_filesize) {
+            #檔案大小正確
+        } else {
+            #檔案大小不正確
+        }
+
+    } else {
+        #日期不正確
+    }
+
+} else {
+    #檔案不存在 或者 網路有問題
+}
