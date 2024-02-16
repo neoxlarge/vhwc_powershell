972b31ae902bbbcbb32ac384b80d97cbea918ee6
neoxlarge_aio <neoxlarge@gmail.com>
2024-02-16 23:03:46 +0800

    save

--- check-systemback2.ps1
+++ check-systemback2.ps1
@@ -279,16 +279,16 @@ foreach ($r in $check_report.keys) {
     if ($check_allpass) {
         $msg = "🟢 Pass: " + $r.Split('_')[0] + "`n" +
                 "path: " + $check_report[$r]['file_path'] + "`n" +
-                "date: " + $check_report[$r]['file_date'] + "`n" +
-                "size: " + $check_report[$r]['file_size'] / 1024 / 1024 + "kb `n" +
-                "------------"
+                #"date: " + $check_report[$r]['file_date'] + "`n" +
+                #"size: " + $check_report[$r]['file_size'] / 1024 / 1024 + "kb `n" +
+                "------------ `n"
 
     } else {
         $msg = "💩 Fail: " + $r.Split('_')[0] + "`n" +
                 "path: " + $check_report[$r]['file_path'] + "`n" +
                 "date: " + $check_report[$r]['file_date'] + "`n" +
-                "size: " + $check_report[$r]['file_size'] / 1024 / 1024 + "kb `n" +
-                "------------"
+                "size: " + $check_report[$r]['file_size'] + "`n" +
+                "------------ `n"
     }
 
     $send_msg += $msg
