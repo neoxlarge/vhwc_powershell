7adb9831ff95e948190f251b3dcfc767e3343817
neoxlarge_aio <neoxlarge@gmail.com>
2024-02-27 19:00:49 +0800

    dd

--- test.py
+++ test.py
@@ -1,8 +1,17 @@
 from selenium import webdriver
+import pandas as pd
+
 
 # https://g.co/gemini/share/ada92acb29a0
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
 
 driver = webdriver.Edge(options=options)
-sss = driver.get("https://www.google.com.tw")
+driver.get("https://allapp.vhcy.gov.tw/webreg/frmOpdSchedule_wc")
+
+
+table = pd.read_html(driver.page_source)[0]
+
+print(table)
+
+
