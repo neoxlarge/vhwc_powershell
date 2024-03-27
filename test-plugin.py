d34fc637e26c0763b0069a263947394f69d44d3a
neoxlarge_aio <neoxlarge@gmail.com>
2024-03-27 22:03:11 +0800

    update

--- test-plugin.py
+++
@@ -1,3 +0,0 @@
-﻿aaa =  open("d:\\mis\\webdriver\\vhcy_cpoe_20240311205152.png", "rb")
-
-print(aaa.tostring)
--- test-url.ps1
+++
@@ -1,4 +0,0 @@
-
-
-$driver = Start-SeChrome
-Enter-SeUrl "http://172.20.200.71/eroe/m2/batch?" -Driver $driver
