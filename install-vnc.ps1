c9fa77db04436571f8ebb8c9af6e1d2c70e51a30
vhwc73058 <vhwc73058@outlook.com>
2024-07-17 17:06:31 +0800

    #多建立一個公用桌面的捷徑vncviewer

--- install-vnc.ps1
+++ install-vnc.ps1
@@ -3,18 +3,83 @@
 
 param($runadmin)
 
-Import-Module ((Split-Path $PSCommandPath) + "\get-installedprogramlist.psm1")
+# import vhwcmis_module.psm1
+# 取得vhwcmis_module.psm1的3種方式:
+# 1.程式執行當前路徑, 放到AD上用Group police執行,不會有當前路徑.
+# 2.常用的路徑, d:\mis\vhwc_powershell, 不是每台都有放.
+# 3.連到NAS上取得. 非網域的電腦會沒有NAS的權限, 須手動連上NAS.
+
+$log_file = "\\172.20.1.14\update\0001-中榮系統環境設定\VHWC_logs\update-VNC.log"
+
+$pspaths = @()
+
+$work_path = "$(Split-Path $script:MyInvocation.MyCommand.Path)\vhwcmis_module.psm1"
+if (test-path -Path $work_path) { $pspaths += $work_path }
+
+$nas_name = "nas122"
+$nas_path = "\\172.20.1.122\share\software\00newpc\vhwc_powershell"
+if (!(test-path $nas_path)) {
+    $nas_Username = "software_download"
+    $nas_Password = "Us2791072"
+    $nas_securePassword = ConvertTo-SecureString $nas_Password -AsPlainText -Force
+    $nas_credential = New-Object System.Management.Automation.PSCredential($nas_Username, $nas_securePassword)
+    
+    New-PSDrive -Name $nas_name -Root "$nas_path" -PSProvider FileSystem -Credential $nas_credential | Out-Null
+}
+$pspaths += "$nas_path\vhwcmis_module.psm1"
+
+$local_path = "d:\mis\vhwc_powershell\vhwcmis_module.psm1"
+if (Test-Path $local_path) { $pspaths += $local_path }
+
+foreach ($path in $pspaths) {
+    Import-Module $path -ErrorAction SilentlyContinue
+    if ((get-command -Name "get-installedprogramlist" -CommandType Function -ErrorAction SilentlyContinue)) {
+        break
+    }
+}
+
 
 function install-VNC {
     
     $software_name = "UltraVnc"
-    $software_path = "\\172.20.5.187\mis\08-VNC\1_2_24"
-    $software_msi = "UltraVNC_1_2_24_X64_Setup.exe"
-    $software_msi_x86 = "UltraVNC_1_2_24_X86_Setup.exe"
+    $software_path = "\\172.20.1.122\share\software\00newpc\08-VNC\1_4_36"
+    $software_msi = "UltraVNC_X64.msi"
+    $software_msi_x86 = "UltraVNC_X86.msi"
+    $log_file = "\\172.20.1.14\update\0001-中榮系統環境設定\VHWC_logs\update-VNC.log"
+
+    ## 判斷OS是32(x86)或是64(AMD64), 其他值(ARM64)不安裝  
+    switch ($env:PROCESSOR_ARCHITECTURE) {
+        "AMD64" { $software_exec = $software_msi }
+        "x86" { $software_exec = $software_msi_x86 }
+        default { Write-Warning "Unsupport CPU or OS:"  $env:PROCESSOR_ARCHITECTURE; $software_exec = $null }
+    }
+
     ## 找出軟體是否己安裝
 
     $all_installed_program = get-installedprogramlist
     $software_is_installed = $all_installed_program | Where-Object -FilterScript { $_.DisplayName -like $software_name }
+
+
+    if ($software_is_installed) {
+
+        $msi_version = get-msiversion -MSIPATH "$software_path\$software_exec"
+        $result = Compare-Version -Version1 $msi_version -Version2 $software_is_installed.DisplayVersion
+
+        if ($result) {
+            $ipv4 = Get-IPv4Address 
+            Write-Log -logfile $log_file -message "Find old VNC version:$($software_is_installed.DisplayVersion)"
+  
+            Write-Output "Find old VNC $software_name, version: $($software_is_installed.DisplayVersion)"
+            Write-Output "Removing old version."
+            Start-Process -FilePath $software_is_installed.UninstallString -ArgumentList "/SILENT" -Wait
+            $software_is_installed = $null
+
+        } else {
+            $msg = "Installed VNC: $($software_is_installed.DisplayVersion)"
+            Write-Log -logfile $log_file -message $msg
+        
+        }
+    }
 
     if ($null -eq $software_is_installed) {
     
@@ -24,16 +89,33 @@ function install-VNC {
         $software_path = get-item -Path $software_path
         Copy-Item -Path $software_path -Destination $env:temp -Recurse -Force -Verbose
 
-        ## 判斷OS是32(x86)或是64(AMD64), 其他值(ARM64)不安裝  
-        switch ($env:PROCESSOR_ARCHITECTURE) {
-            "AMD64" { $software_exec = $software_msi }
-            "x86" { $software_exec = $software_msi_x86 }
-            default { Write-Warning "Unsupport CPU or OS:"  $env:PROCESSOR_ARCHITECTURE; $software_exec = $null }
-        }
+        <#
+        SERVERVIEWER
+        1 server
+        2 viewer
+        3 server + viewer
+        SERVICE
+        1 install
+        2 not install
+
+        PASSWORD = mypassword
+        Sample
+        UltraVNC_1436_X86.msi  SERVERVIEWER=3 SERVICE=1 PASSWORD="sysc0012"
+        #>
+
 
         if ($null -ne $software_exec) {
-            Start-Process -FilePath $($env:temp + "\" + $software_path.Name + "\" + $software_exec) -ArgumentList "/silent /loadinf=$($env:temp + "\" + $software_path.Name + "\installvnc.inf")" -Wait
-            Start-Sleep -Seconds 5 
+            #Start-Process -FilePath $($env:temp + "\" + $software_path.Name + "\" + $software_exec) -ArgumentList "/silent /loadinf=$($env:temp + "\" + $software_path.Name + "\installvnc.inf")" -Wait
+            $install_filepath = "$($env:temp)\$($software_path.Name)\$software_exec"
+
+            $install_arrg = "/i $install_filepath /passive /norestart PASSWORD=""sysc0012"" SERVERVIEWER=3 SERVICE=1"
+            
+            Start-Process -FilePath "msiexec.exe" -ArgumentList $install_arrg -Wait
+
+            
+            Write-Log -logfile $log_file -message "Start to install VNC: $install_filepath"
+
+            Start-Sleep -Seconds 5
         }
         else {
             Write-Warning "$software_name 無法正常安裝."
@@ -41,6 +123,11 @@ function install-VNC {
       
         #復制設定檔vltravnc.ini 到C:\Program Files\uvnc bvba\UltraVNC
         Copy-Item -Path ($env:temp + "\" + $software_path.Name + "\ultravnc.ini") -Destination ($env:ProgramFiles + "\uvnc bvba\UltraVNC") -Force
+
+        #多建立一個公用桌面的捷徑
+        $shortcutPath = "c:\Users\Public\Desktop\VNC Viewer.lnk"
+        $targetPath = "C:\Program Files\uvnc bvba\UltraVNC\vncviewer.exe"
+        New-Item -ItemType SymbolicLink -Path $shortcutPath -Target $targetPath -ErrorAction SilentlyContinue
 
         #安裝完, 再重新取得安裝資訊
         $all_installed_program = get-installedprogramlist
@@ -74,7 +161,8 @@ if ($run_main -eq $null) {
     else {
         Write-Warning "無法取得管理員權限來安裝軟體, 請以管理員帳號重試."
     }
-    pause
+    #pause
+    Start-Sleep -Seconds 10
 }
 
 
