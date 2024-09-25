ed66e9289ac106ac26cbdbace2f51d7f784a6156
neoxlarge <neoxlarge@gmail.com>
2024-09-26 01:30:09 +0800

    update

--- dutycheck-selenium.ps1
+++ dutycheck-selenium.ps1
@@ -10,9 +10,38 @@ https://www.zenrows.com/blog/selenium-powershell#interaction-automation
 
 Import-Module selenium
 
+$check_cpoe = @{
+    'vhwc_cpoe' = @{
+        'branch' = "vhwc"
+        'url' = 'http://172.20.200.71/cpoe/m2/batch'
+        'account' = 'vhwcmis'
+        'password' = 'Mis20190610'
+    }
+    'vhcy_cpoe' = @{
+        'branch' = "vhcy"
+        'url' = 'http://172.19.200.71/cpoe/m2/batch'
+        'account' = 'vhwcmis'
+        'password' = 'Mis20190610'
+    }
+}
+
+
+function check-oe( $branch, $url, $account, $password ) {
+
+
+    $driver = Start-SeChrome -WebDriverDirectory ".\" -headless
+
+    Enter-SeUrl -Url $url -Driver $Driver
+
+    $driver.GetScreenshot( ).SaveAsFile( "screenshotxxx.png", "png" )
+
+    Stop-SeDriver -Driver $Driver
+
+}
+
 $driver = Start-SeChrome -WebDriverDirectory ".\" -headless 
 
-$driver | gm
+
 
 Enter-SeUrl 'https://www.google.com.tw/' -Driver $Driver
 
