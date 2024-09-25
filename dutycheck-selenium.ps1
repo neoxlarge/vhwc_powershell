e01184fd0e69e8440620f6716db6d2448f757d8f
vhwc73058 <vhwc73058@outlook.com>
2024-09-25 17:31:26 +0800

    update

--- dutycheck-selenium.ps1
+++ dutycheck-selenium.ps1
@@ -1,9 +1,22 @@
+<#
+selenium 使用方法
+https://github.com/adamdriscoll/selenium-powershell
+https://www.zenrows.com/blog/selenium-powershell#interaction-automation
+
+
+
+
+#>
+
 Import-Module selenium
 
-$driver = Start-SeChrome -WebDriverDirectory ".\" -headless
+$driver = Start-SeChrome -WebDriverDirectory ".\" -headless 
+
+$driver | gm
 
 Enter-SeUrl 'https://www.google.com.tw/' -Driver $Driver
 
+$driver
 #$Html = $Driver.PageSource
 #$Html
 
