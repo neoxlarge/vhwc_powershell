826fe951caedd4370a2b7efacf66fa3f70d9235e
neoxlarge <neoxlarge@gmail.com>
2024-09-28 20:18:43 +0800

    update

--- dutycheck-selenium.ps1
+++ dutycheck-selenium.ps1
@@ -3,12 +3,25 @@ selenium 使用方法
 https://github.com/adamdriscoll/selenium-powershell
 https://www.zenrows.com/blog/selenium-powershell#interaction-automation
 
-
+chromedrive.exe 下載位置
+https://googlechromelabs.github.io/chrome-for-testing/#stable
 
 
 #>
 
-Import-Module selenium
+
+# 儲存截圖和網頁檔的路徑
+$result_path = "\\172.20.5.185\mis\dutycheck_result"
+
+# chromedrive 路徑, 此powershell預計會到遠端桌面主機上執行, 所以要確認遠端主機上的路徑.
+# 預計是放在 d:\mis\vhwc_powershell\chromedriver.exe
+$chromedriver_path = "d:\mis\vhwc_powershell\"
+
+# selenium module path
+# 遠端桌面主機未安裝selenium powershell 模組, 用匯入的方式載入模組.
+Import-Module "d:\mis\vhwc_powershell\selenium\3.15.0\selenium.psd1"
+
+
 
 $check_oe = @{
     'vhwc_cpoe' = @{
@@ -48,10 +61,10 @@ $check_oe = @{
 
 
 
-function check-oe( $check_item, $branch, $url, $account, $password, $save_path, $capture_area) {
+function check-oe( $check_item, $branch, $url, $account, $password,  $capture_area) {
 
     # 開啟瀏覽器, headless 模式
-    $driver = Start-SeChrome -WebDriverDirectory ".\" -headless
+    $driver = Start-SeChrome -WebDriverDirectory $chromedriver_path -headless
     # 開啟網址
     Enter-SeUrl -Url $url -Driver $Driver
 
@@ -61,12 +74,7 @@ function check-oe( $check_item, $branch, $url, $account, $password, $save_path, 
     $driver.FindElementByXPath("//input[@name='m2Login_submit']").Click()
     Start-Sleep -Seconds 3
 
-    # 取得登入後的頁面的大小
-    #$width = $driver.ExecuteScript("return document.documentElement.scrollWidth")
-    #$height = $driver.ExecuteScript("return document.documentElement.scrollHeight")
-    
     # 調整視窗大小, 用以全螢幕截圖
-    #$driver.Manage().Window.Size = New-Object System.Drawing.Size(($width + 120), ($height+200))
     $driver.Manage().Window.Size = New-Object System.Drawing.Size(1920, 1080)
 
     # 從capture_area 判斷,網頁要捲動到的位置, 以sendkey home, end 實作
@@ -76,8 +84,8 @@ function check-oe( $check_item, $branch, $url, $account, $password, $save_path, 
     Start-Sleep -Seconds 1
     
     # 儲存頁面和截圖
-    $driver.PageSource | Out-File -FilePath "$($save_path)\$($check_item)_$($branch)_$($date).html"
-    $driver.GetScreenshot( ).SaveAsFile("$($save_path)\$($check_item)_$($branch)_$($date).png", "png")
+    $driver.PageSource | Out-File -FilePath "$($result_path)\$($check_item)_$($branch)_$($date).html"
+    $driver.GetScreenshot( ).SaveAsFile("$($result_path)\$($check_item)_$($branch)_$($date).png", "png")
 
     # 關閉瀏覽器 
     Stop-SeDriver -Driver $Driver
@@ -86,8 +94,8 @@ function check-oe( $check_item, $branch, $url, $account, $password, $save_path, 
     $result = @{"check_item" = $check_item; 
         "branch"             = $branch; 
         "date"               = $date; 
-        "png_filepath"       = "$($save_path)\$($check_item)_$($branch)_$($date).png";
-        "html_filepath"      = "$($save_path)\$($check_item)_$($branch)_$($date).html"
+        "png_filepath"       = "$($result_path)\$($check_item)_$($branch)_$($date).png";
+        "html_filepath"      = "$($result_path)\$($check_item)_$($branch)_$($date).html"
     }
 
     return $result        
@@ -111,23 +119,17 @@ $check_showjob = @{
         
 }
 
-function check-showjob ($check_item, $branch, $url, $save_path) {
+function check-showjob ($check_item, $branch, $url) {
 
     # 開啟瀏覽器, headless 模式
-    $driver = Start-SeChrome -WebDriverDirectory ".\" -headless
+    $driver = Start-SeChrome -WebDriverDirectory $chromedriver_path -headless
     # 開啟網址
     Enter-SeUrl -Url $url -Driver $Driver
 
     $driver.FindElementByXPath("//input[@id='btnExec']").Click()
+    start-sleep -second 2
 
-    start-sleep -second 5
-
-    # 取得登入後的頁面的大小
-    #$width = $driver.ExecuteScript("return document.documentElement.scrollWidth")
-    #$height = $driver.ExecuteScript("return document.documentElement.scrollHeight")
-    
     # 調整視窗大小, 用以全螢幕截圖
-    #$driver.Manage().Window.Size = New-Object System.Drawing.Size(($width + 120), ($height+200))
     $driver.Manage().Window.Size = New-Object System.Drawing.Size(1920, 1080)
     
     # 從capture_area 判斷,網頁要捲動到的位置, 以sendkey home, end 實作
@@ -136,8 +138,8 @@ function check-showjob ($check_item, $branch, $url, $save_path) {
     start-sleep -second 1
     
     # 儲存頁面和截圖
-    $driver.PageSource | Out-File -FilePath "$($save_path)\$($check_item)_$($branch)_$($date).html"
-    $driver.GetScreenshot( ).SaveAsFile( "$($save_path)\$($check_item)_$($branch)_$($date).png", "png" )
+    $driver.PageSource | Out-File -FilePath "$($result_path)\$($check_item)_$($branch)_$($date).html"
+    $driver.GetScreenshot( ).SaveAsFile( "$($result_path)\$($check_item)_$($branch)_$($date).png", "png" )
 
     # 關閉瀏覽器 
     Stop-SeDriver -Driver $driver
@@ -145,8 +147,8 @@ function check-showjob ($check_item, $branch, $url, $save_path) {
     # 回傳結果
     $result = @{"check_item" = $check_item; 
         "branch"             = $branch; 
-        "png_filepath"       = "$($save_path)\$($check_item)_$($branch)_showjob.png";
-        "html_filepath"      = "$($save_path)\$($check_item)_$($branch)_showjob.html"
+        "png_filepath"       = "$($result_path)\$($check_item)_$($branch)_showjob.png";
+        "html_filepath"      = "$($result_path)\$($check_item)_$($branch)_showjob.html"
     }
     return $result        
 }
@@ -172,11 +174,11 @@ $check_cyp2001 = @{
     }
 }
 
-function check-cyp2001 ($check_item, $branch, $url_login, $url_query, $account, $password, $save_path) {
+function check-cyp2001 ($check_item, $branch, $url_login, $url_query, $account, $password) {
 
     # 報表系統要先登入,才能查詢
     # 開啟瀏覽器, headless 模式
-    $driver = Start-SeChrome -WebDriverDirectory ".\" -headless
+    $driver = Start-SeChrome -WebDriverDirectory $chromedriver_path -headless
     # 開啟網址
     Enter-SeUrl -Url $url_login -Driver $Driver
 
@@ -184,9 +186,9 @@ function check-cyp2001 ($check_item, $branch, $url_login, $url_query, $account, 
     $driver.FindElementByXPath("//input[@name='cn']").SendKeys($account)
     $driver.FindElementByXPath("//input[@name='pw']").SendKeys($password)
     $driver.FindElementByXPath("//input[@type='submit']").Click()
-    #Send-SeKeys -Keys 'Return' -Driver $Driver
     
-    # 日期轉換為民國年+月+日, 例如1130926
+    
+    # 日期轉換為民國年/月/日, 例如113/09/26
     # 取得今天的日期
     $Today = Get-Date
     # 計算民國年
@@ -199,21 +201,23 @@ function check-cyp2001 ($check_item, $branch, $url_login, $url_query, $account, 
         'g_yyymmdd_s' = $TaiwanDate
         'from'        = $branch
     }
+    
+    # 報表查詢會花一些時間跑
     $result = Invoke-WebRequest -uri $url_query -Method POST -Body $body
-    Start-Sleep -Seconds 5
+    Start-Sleep -Seconds 2
 
-    # 網頁存檔
-    $result.Content | Out-File -FilePath "$($save_path)\$($check_item)_$($branch)_$($date).html"
-    Start-Sleep -Seconds 5
-    Enter-SeUrl -Url "$($save_path)\$($check_item)_$($branch)_$($date).html" -Driver $driver
+    # 網頁存檔後再讀取
+    $result.Content | Out-File -FilePath "$($result_path)\$($check_item)_$($branch)_$($date).html"
+    Enter-SeUrl -Url "$($result_path)\$($check_item)_$($branch)_$($date).html" -Driver $driver
 
+    # 取得視窗大小, 嘉義的比較長, 怕截到表格.
     $width = $driver.ExecuteScript("return document.documentElement.scrollWidth")
     $height = $driver.ExecuteScript("return document.documentElement.scrollHeight")
     
     # 調整視窗大小, 用以全螢幕截圖
-    $driver.Manage().Window.Size = New-Object System.Drawing.Size(($width + 120), ($height + 200))
+    $driver.Manage().Window.Size = New-Object System.Drawing.Size(($width), ($height))
     # 截圖存檔
-    $driver.GetScreenshot( ).SaveAsFile( "$($save_path)\$($check_item)_$($branch)_$($date).png", "png" )
+    $driver.GetScreenshot( ).SaveAsFile( "$($result_path)\$($check_item)_$($branch)_$($date).png", "png" )
 
     # 關閉瀏覽器 
     Stop-SeDriver -Driver $driver
@@ -221,8 +225,8 @@ function check-cyp2001 ($check_item, $branch, $url_login, $url_query, $account, 
     # 回傳結果
     $result = @{"check_item" = $check_item;
         "branch"             = $branch;
-        "png_filepath"       = "$($save_path)\$($check_item)_$($branch)_$($date).png";
-        "html_filepath"      = "$($save_path)\$($check_item)_$($branch)_$($date).html"
+        "png_filepath"       = "$($result_path)\$($check_item)_$($branch)_$($date).png";
+        "html_filepath"      = "$($result_path)\$($check_item)_$($branch)_$($date).html"
     }
     return $result
 }
@@ -233,13 +237,13 @@ $date = (get-date).ToString('yyyyMMddhhmm')
 
 
 foreach ($key in $check_oe.keys) {
-    check-oe -check_item $key -branch $check_oe[$key]['branch'] -url $check_oe[$key]['url'] -account $check_oe[$key]['account'] -password $check_oe[$key]['password'] -save_path 'd:\mis' -capture_area $check_oe[$key]['capture_area']
+    check-oe -check_item $key -branch $check_oe[$key]['branch'] -url $check_oe[$key]['url'] -account $check_oe[$key]['account'] -password $check_oe[$key]['password'] -capture_area $check_oe[$key]['capture_area']
 }
 
 foreach ($key in $check_showjob.keys) {
-    check-showjob -check_item $key -branch $check_showjob[$key]['branch'] -url $check_showjob[$key]['url'] -save_path 'd:\mis' -capture_area $check_showjob[$key]['capture_area']
+    check-showjob -check_item $key -branch $check_showjob[$key]['branch'] -url $check_showjob[$key]['url']  -capture_area $check_showjob[$key]['capture_area']
 }
 
 foreach ($key in $check_cyp2001.keys) {
-    check-cyp2001 -check_item $key -branch $check_cyp2001[$key]['branch'] -account $check_cyp2001[$key]['account'] -password $check_cyp2001[$key]['password'] -url_login $check_cyp2001[$key]['url_login'] -url_query $check_cyp2001[$key]['url_query'] -save_path 'd:\mis'
+    check-cyp2001 -check_item $key -branch $check_cyp2001[$key]['branch'] -account $check_cyp2001[$key]['account'] -password $check_cyp2001[$key]['password'] -url_login $check_cyp2001[$key]['url_login'] -url_query $check_cyp2001[$key]['url_query'] 
 }
