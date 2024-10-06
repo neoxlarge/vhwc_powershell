f9b859eafe4a9c2ca0a86b4919e06138bd1bdca0
neoxlarge <neoxlarge@gmail.com>
2024-10-06 10:58:41 +0800

    coding dutycheck-servproc

---
+++ dutycheck-ServProc.ps1
@@ -0,0 +1,37 @@
+# 檢查Server上的程式是否當機
+
+
+$server_list = [ordered]@{
+    'transform1' = @{
+        'computername' = 'Blade64-Srv3-wc';
+        'ip' = '172.20.200.41';
+        'processes' = @('hdste02prj.exe',
+                        'hdste03prj.exe',
+                        'hdste04prj.exe',
+                        'hdste05prj.exe',
+                        'hdste06prj.exe',
+                        'hdste07prj.exe',
+                        'hdstq09prj.exe',
+                        'hdstq10prj.exe');
+        'account' = 'user';
+        'password' = 'acervghtc'
+    }
+}
+
+$server= $server_list.transform1
+
+$Username = ".\$($server.account)"
+$Password = "$server.password"
+$securePassword = ConvertTo-SecureString $Password -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential($Username, $securePassword)
+
+
+$processes = Get-WmiObject -ComputerName $server.ip -Credential $credential -class win32_process 
+
+$processes = Where-Object -InputObject $processes -FilterScript {$_.Name -in $server.processes}
+
+# 1.檢查程式數量是否正確, 如果不對, 找出少那一個
+if ($processes.count -ne $server.processes.count) {
+    $missingProcesses = Compare-Object -ReferenceObject $server.processes -DifferenceObject $processes.Name -IncludeEqual -ExcludeDifferent 
+    Write-Host "Missing processes: $missingProcesses" -ForegroundColor Red
+}
