c2fcdfa5738d5f32272d3b3e2e5384db85c54283
vhwc73058 <vhwc73058@outlook.com>
2024-10-07 17:34:58 +0800

    code servproc

--- dutycheck-ServProc.ps1
+++ dutycheck-ServProc.ps1
@@ -12,8 +12,7 @@ $server_list = [ordered]@{
                         'hdste06prj.exe',
                         'hdste07prj.exe',
                         'hdstq09prj.exe',
-                        'hdstq10prj.exe',
-                        'xxxx_test_xxx.exe');
+                        'hdstq10prj.exe');
         'account' = 'opdvghtc';
         'password' = 'acervghtc'
     }
@@ -27,11 +26,50 @@ $securePassword = ConvertTo-SecureString $Password -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential($Username, $securePassword)
 
 
+ $datatable = New-Object System.Data.DataTable
+ $datatable.Columns.add('computername', [string]) | Out-Null
+ $datatable.Columns.add('ip', [string]) | Out-Null
+ $datatable.Columns.add('resonseDateTime', [string]) | Out-Null
+ $datatable.Columns.add('processName', [string]) | Out-Null
+ #$datatable.Columns.add('status', [string]) | Out-Null
+ $datatable.Columns.add('processid', [string]) | Out-Null
+ $datatable.Columns.add('workingsetsize', [string]) | Out-Null
+ $datatable.Columns.add('ThreadCount', [string]) | Out-Null
+ $datatable.Columns.add('HandleCount', [string]) | Out-Null
+ $datatable.Columns.add('cpuUsage', [string]) | Out-Null
+
+do {
+    
+
 $processes = Get-WmiObject -ComputerName $server.ip -Credential $credential -class win32_process 
-$processes = $processes |Where-Object -FilterScript {$_.Name -in $server.processes}
+$processes = $processes |Where-Object -FilterScript {$_.Name -in $server.processes} | Select-Object -Property processid, name, workingsetsize, ThreadCount, HandleCount
 
 # 1.檢查程式數量是否正確, 如果不對, 找出少那一個
 if ($processes.count -ne $server.processes.count) {
     $missingProcesses = Compare-Object -ReferenceObject $server.processes -DifferenceObject $processes.Name #-IncludeEqual -ExcludeDifferent 
     Write-Host "Missing processes: $($missingProcesses.inputobject)" -ForegroundColor Red
 }
+
+$cpuUsage = Get-WmiObject -Class Win32_PerfFormattedData_PerfProc_Process -ComputerName $server.ip -Credential $credential |
+            Where-Object { $_.IDProcess -in $processes.processid } 
+
+foreach ($process in $processes) {
+    # 在$cpuUsage中找到相同的processid
+    $cpuUsage_match = $cpuUsage | Where-Object { $_.IDProcess -eq $process.processid }
+    
+    if ($cpuUsage_match -ne $null) {
+        $datatable.Rows.Add($server.computername, $server.ip, (Get-Date), $process.name, $process.processid, $process.workingsetsize, $process.ThreadCount, $process.HandleCount, $cpuUsage_match.PercentProcessorTime) | Out-Null
+    }
+    else {
+        $datatable.Rows.Add($server.computername, $server.ip, (Get-Date), $process.name, $process.processid, $process.workingsetsize, $process.ThreadCount, $process.HandleCount, 'none') | Out-Null
+    }
+}
+
+
+Clear-Host
+$datatable | format-table 
+Start-Sleep -s 60
+}
+while (
+    $true<# Condition that stops the loop if it returns false #>
+)
