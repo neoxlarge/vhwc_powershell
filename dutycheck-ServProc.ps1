2c089aa16e9f329f3cc885d0bdbd6d01615153ad
vhwc73058 <vhwc73058@outlook.com>
2024-10-09 18:35:06 +0800

    update

--- dutycheck-ServProc.ps1
+++ dutycheck-ServProc.ps1
@@ -64,8 +64,8 @@ $server_list = [ordered]@{
         'processes'    = @('ERClient.exe',
             'pycharm64.exe',
             'py.exe')
-        'account'      = 'Activity!';
-        'password'     = 'Activity!'                
+        'account'      = 'Administrator';
+        'password'     ='Acervghtc!'                
 
     }
 
@@ -159,39 +159,39 @@ do {
     
 
 
-    foreach ($server in $server_list ) {
+    foreach ($server in $server_list.Keys ) {
 
         #先檢查server的連線
-        $ping = Test-Connection -ComputerName $server.ip -Count 1 -Quiet
+        $ping = Test-Connection -ComputerName $server_list[$server].ip -Count 1 -Quiet
         if ($ping) {
-            write-debug "$($server.title): $($server.ip) 連線成功"
+            write-debug "$($server_list[$server].title): $($server_list[$server].ip) 連線成功"
         }
         else {
-            Write-Debug "$($server.title): $($server.ip) 連線失敗"
-            # Send-LineNotifyMessage -Message "$($server.title): $($server.ip) 連線失敗" -StickerPackageId 1 -StickerId 100
+            Write-Debug "$($server_list[$server].title): $($server_list[$server].ip) 連線失敗"
+            # Send-LineNotifyMessage -Message "$($server_list[$server].title): $($server_list[$server].ip) 連線失敗" -StickerPackageId 1 -StickerId 100
             continue
         }
 
         if ($ping) {
 
-            $Username = ".\$($server.account)"
-            $Password = "$($server.password)"
+            $Username = ".\$($server_list[$server].account)"
+            $Password = "$($server_list[$server].password)"
             $securePassword = ConvertTo-SecureString $Password -AsPlainText -Force
             $credential = New-Object System.Management.Automation.PSCredential($Username, $securePassword)
 
             # 連線到遠端電腦並取得執行中的程式
-            Write-Debug "連線到 $($server.ip) 並取得執行中的程式..."
-            $processes = Get-WmiObject -ComputerName $server.ip -Credential $credential -class win32_process 
-            $processes = $processes | Where-Object -FilterScript { $_.Name -in $server.processes } | Select-Object -Property processid, name, workingsetsize, ThreadCount, HandleCount
+            Write-Debug "連線到 $($server_list[$server].ip) 並取得執行中的程式..."
+            $processes = Get-WmiObject -ComputerName $server_list[$server].ip -Credential $credential -class win32_process 
+            $processes = $processes | Where-Object -FilterScript { $_.Name -in $server_list[$server].processes } | Select-Object -Property processid, name, workingsetsize, ThreadCount, HandleCount
             # 1.檢查程式數量是否正確, 如果不對, 找出少那一個
-            if ($processes.count -ne $server.processes.count) {
-                $missingProcesses = Compare-Object -ReferenceObject $server.processes -DifferenceObject $processes.Name #-IncludeEqual -ExcludeDifferent 
+            if ($processes.count -ne $server_list[$server].processes.count) {
+                $missingProcesses = Compare-Object -ReferenceObject $server_list[$server].processes -DifferenceObject $processes.Name #-IncludeEqual -ExcludeDifferent 
                 Write-Host "Missing processes: $($missingProcesses.inputobject)" -ForegroundColor Red
             }
 
             # 連線到遠端電腦並取得指定程式的 CPU 使用率
-            Write-Debug "連線到 $($server.ip) 並取得指定程式的 CPU 使用率..."
-            $cpuUsage = Get-WmiObject -Class Win32_PerfFormattedData_PerfProc_Process -ComputerName $server.ip -Credential $credential |
+            Write-Debug "連線到 $($server_list[$server].ip) 並取得指定程式的 CPU 使用率..."
+            $cpuUsage = Get-WmiObject -Class Win32_PerfFormattedData_PerfProc_Process -ComputerName $server_list[$server].ip -Credential $credential |
             Where-Object { $_.IDProcess -in $processes.processid } 
 
             foreach ($process in $processes) {
@@ -202,11 +202,11 @@ do {
         
                 # 如果找到, 就加入datatable
                 if ($cpuUsage_match -ne $null) {
-                    $datatable.Rows.Add($server.computername, $server.ip, (Get-Date), $process.name, $process.processid, $process.workingsetsize, $process.ThreadCount, $process.HandleCount, $cpuUsage_match.PercentProcessorTime) | Out-Null
+                    $datatable.Rows.Add($server_list[$server].computername, $server_list[$server].ip, (Get-Date), $process.name, $process.processid, $process.workingsetsize, $process.ThreadCount, $process.HandleCount, $cpuUsage_match.PercentProcessorTime) | Out-Null
                 }
                 else {
                     # 如果沒找到, 就加入datatable, cpuUsage欄位填入'none'
-                    $datatable.Rows.Add($server.computername, $server.ip, (Get-Date), $process.name, $process.processid, $process.workingsetsize, $process.ThreadCount, $process.HandleCount, 'none') | Out-Null
+                    $datatable.Rows.Add($server_list[$server].computername, $server_list[$server].ip, (Get-Date), $process.name, $process.processid, $process.workingsetsize, $process.ThreadCount, $process.HandleCount, 'none') | Out-Null
                 }
     
                 # 找出指定的程式, 並且按照resonseDateTime排序, 取出最新的2筆資料
@@ -216,7 +216,7 @@ do {
                 # 表示程式可能當機了
                 $last2rows = $sortedtable | Select-Object -First 2
         
-                $last2rows | Format-Table -AutoSizes
+                $last2rows | Format-Table 
         
                 if (($last2rows.Count -eq 2) -and ($last2rows[0].processName -eq $last2rows[1].processName) -and ($last2rows[0].workingsetsize -eq $last2rows[1].workingsetsize) -and ($last2rows[0].ThreadCount -eq $last2rows[1].ThreadCount) -and ($last2rows[0].HandleCount -eq $last2rows[1].HandleCount)) {
                     Write-Host "Warning: $($last2rows[0].processName) on $($last2rows[0].computername) may be crashed." -ForegroundColor Yellow
