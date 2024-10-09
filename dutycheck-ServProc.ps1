6e52034f065e1153b9f27507067c1c8afbdeda89
neoxlarge <neoxlarge@gmail.com>
2024-10-09 15:34:14 +0800

    test

--- dutycheck-ServProc.ps1
+++ dutycheck-ServProc.ps1
@@ -4,96 +4,98 @@ $DebugPreference = 'Continue'
 
 $server_list = [ordered]@{
     'transform1' = @{
-        'title' = '檢驗科轉檔'
+        'title'        = '檢驗科轉檔'
         'computername' = 'Blade64-Srv3-wc';
-        'ip' = '172.20.200.41';
-        'processes' = @('hdste02prj.exe',
-                        'hdste03prj.exe',
-                        'hdste04prj.exe',
-                        'hdste05prj.exe',
-                        'hdste06prj.exe',
-                        'hdste07prj.exe',
-                        'hdstq09prj.exe',
-                        'hdstq10prj.exe');
-        'account' = 'opdvghtc';
-        'password' = 'acervghtc'
+        'ip'           = '172.20.200.41';
+        'processes'    = @('hdste02prj.exe',
+            'hdste03prj.exe',
+            'hdste04prj.exe',
+            'hdste05prj.exe',
+            'hdste06prj.exe',
+            'hdste07prj.exe',
+            'hdstq09prj.exe',
+            'hdstq10prj.exe');
+        'account'      = 'opdvghtc';
+        'password'     = 'acervghtc'
     }
 
     'transform2' = @{
-        'title' = '儀器轉檔(小豬程式)';
+        'title'        = '儀器轉檔(小豬程式)';
         'computername' = 'wser-005-cloudmed';
-        'ip' = '172.20.1.5';
-        'processes' = @('ep.exe',
-                        '06-新舊his系統檢驗報告回傳程式APPPRJ .exe',
-                        'CTMRIUpload.exe',
-                        'NHI_EII_View.exe');
-        'account' = 'user';
-        'password' = 'tedpc017E'
+        'ip'           = '172.20.1.5';
+        'processes'    = @('ep.exe',
+            '06-新舊his系統檢驗報告回傳程式APPPRJ .exe',
+            'CTMRIUpload.exe',
+            'NHI_EII_View.exe');
+        'account'      = 'user';
+        'password'     = 'tedpc017E'
 
     }
 
     'transform3' = @{
-        'title' = '傳保卡1.0 上傳程式';
+        'title'       = '傳保卡1.0 上傳程式';
         'coputername' = 'wmis-111-pc01';
-        'ip' = '172.20.1.4';
-        'processes' = @('NHI_EII_View.exe',
-                        'IccPrj.exe',
-                        'PhrB0O0Prj.exe',
-                        'RegB090Prj.exe',
-                        'RegB092Prj.exe',
-                        'RegB093Prj.exe')
-        'account' = 'user';
-        'password' = 'Us2791072'             
+        'ip'          = '172.20.1.4';
+        'processes'   = @('NHI_EII_View.exe',
+            'IccPrj.exe',
+            'PhrB0O0Prj.exe',
+            'RegB090Prj.exe',
+            'RegB092Prj.exe',
+            'RegB093Prj.exe')
+        'account'     = 'user';
+        'password'    = 'Us2791072'             
     }
 
-    'tranform4' = @{
-        'title' = '雲端批次下載';
+    'tranform4'  = @{
+        'title'        = '雲端批次下載';
         'computername' = 'wadm-inx-pc02x';
-        'ip' = '172.20.5.147';
-        'processes' = @('IccPrj.exe',
-                        'HISLogin.exe',
-                        'HISSystem.exe')
-        'account' = 'user';
-        'password' = 'Us2791072'                
+        'ip'           = '172.20.5.147';
+        'processes'    = @('IccPrj.exe',
+            'HISLogin.exe',
+            'HISSystem.exe')
+        'account'      = 'user';
+        'password'     = 'Us2791072'                
     }
 
-    'tranform5' = @{
-        'title' = '急診通報';
+    'tranform5'  = @{
+        'title'        = '急診通報';
         'computername' = 'wadm-in';
-        'ip' = '172.20.200.49'
-        'processes' = @('ERClient.exe',
-                        'pycharm64.exe',
-                        'py.exe')
-        'account' = 'Activity!';
-        'password' = 'Activity!'                
+        'ip'           = '172.20.200.49'
+        'processes'    = @('ERClient.exe',
+            'pycharm64.exe',
+            'py.exe')
+        'account'      = 'Activity!';
+        'password'     = 'Activity!'                
 
     }
 
-    'tranform6' = @{
-        'title' = '會?日結程式';
-        'computername' =  'unknown';
-        'ip' = '172.20.1.3';
-        'processes' = @('attprj.exe',
-                        'NisT010.exe')
-        'account' = 'Administrator';
-        'password' = '279!b4E'
+    'tranform6'  = @{
+        'title'        = '會?日結程式';
+        'computername' = 'unknown';
+        'ip'           = '172.20.1.3';
+        'processes'    = @('attprj.exe',
+            'NisT010.exe')
+        'account'      = 'Administrator';
+        'password'     = '279!b4E'
 
     }
 
 
-    'tranform7' = @{
-        'title' = '警消及榮民';
-        'computername' =  'clonet21';
-        'ip' = '172.20.200.225';
-        'processes' = @('Atcjob.exe',
-                       'AutoMailReport.exe',
-                       'PliVacSFTP.exe',
-                       'DrugAlcoholAddiction.exe',
-                       'cmd.exe')
-        'account' ='vgh00';
-        'password' = 'acervghtc'               
+    'tranform7'  = @{
+        'title'        = '警消及榮民';
+        'computername' = 'clonet21';
+        'ip'           = '172.20.200.225';
+        'processes'    = @('Atcjob.exe',
+            'AutoMailReport.exe',
+            'PliVacSFTP.exe',
+            'DrugAlcoholAddiction.exe',
+            'cmd.exe')
+        'account'      = 'vgh00';
+        'password'     = 'acervghtc'               
     }
 }
+
+
 
 
 function Send-LineNotifyMessage {
@@ -140,79 +142,95 @@ function Send-LineNotifyMessage {
     }
 }
 
+$datatable = New-Object System.Data.DataTable
+$datatable.Columns.add('computername', [string]) | Out-Null
+$datatable.Columns.add('ip', [string]) | Out-Null
+$datatable.Columns.add('resonseDateTime', [Datetime]) | Out-Null
+$datatable.Columns.add('processName', [string]) | Out-Null
+$datatable.Columns.add('processid', [string]) | Out-Null
+$datatable.Columns.add('workingsetsize', [long]) | Out-Null
+$datatable.Columns.add('ThreadCount', [int]) | Out-Null
+$datatable.Columns.add('HandleCount', [int]) | Out-Null
+$datatable.Columns.add('cpuUsage', [int]) | Out-Null
 
 
-
-$server= $server_list.transform1
-
-$Username = ".\$($server.account)"
-$Password = "$($server.password)"
-$securePassword = ConvertTo-SecureString $Password -AsPlainText -Force
-$credential = New-Object System.Management.Automation.PSCredential($Username, $securePassword)
-
-
- $datatable = New-Object System.Data.DataTable
- $datatable.Columns.add('computername', [string]) | Out-Null
- $datatable.Columns.add('ip', [string]) | Out-Null
- $datatable.Columns.add('resonseDateTime', [Datetime]) | Out-Null
- $datatable.Columns.add('processName', [string]) | Out-Null
- #$datatable.Columns.add('status', [string]) | Out-Null
- $datatable.Columns.add('processid', [string]) | Out-Null
- $datatable.Columns.add('workingsetsize', [int32]) | Out-Null
- $datatable.Columns.add('ThreadCount', [int]) | Out-Null
- $datatable.Columns.add('HandleCount', [int]) | Out-Null
- $datatable.Columns.add('cpuUsage', [int]) | Out-Null
 
 do {
     
 
-$processes = Get-WmiObject -ComputerName $server.ip -Credential $credential -class win32_process 
-$processes = $processes |Where-Object -FilterScript {$_.Name -in $server.processes} | Select-Object -Property processid, name, workingsetsize, ThreadCount, HandleCount
 
-# 1.檢查程式數量是否正確, 如果不對, 找出少那一個
-if ($processes.count -ne $server.processes.count) {
-    $missingProcesses = Compare-Object -ReferenceObject $server.processes -DifferenceObject $processes.Name #-IncludeEqual -ExcludeDifferent 
-    Write-Host "Missing processes: $($missingProcesses.inputobject)" -ForegroundColor Red
-}
+    foreach ($server in $server_list ) {
 
-$cpuUsage = Get-WmiObject -Class Win32_PerfFormattedData_PerfProc_Process -ComputerName $server.ip -Credential $credential |
+        #先檢查server的連線
+        $ping = Test-Connection -ComputerName $server.ip -Count 1 -Quiet
+        if ($ping) {
+            write-debug "$($server.title): $($server.ip) 連線成功"
+        }
+        else {
+            Write-Debug "$($server.title): $($server.ip) 連線失敗"
+            # Send-LineNotifyMessage -Message "$($server.title): $($server.ip) 連線失敗" -StickerPackageId 1 -StickerId 100
+            continue
+        }
+
+        if ($ping) {
+
+            $Username = ".\$($server.account)"
+            $Password = "$($server.password)"
+            $securePassword = ConvertTo-SecureString $Password -AsPlainText -Force
+            $credential = New-Object System.Management.Automation.PSCredential($Username, $securePassword)
+
+            # 連線到遠端電腦並取得執行中的程式
+            Write-Debug "連線到 $($server.ip) 並取得執行中的程式..."
+            $processes = Get-WmiObject -ComputerName $server.ip -Credential $credential -class win32_process 
+            $processes = $processes | Where-Object -FilterScript { $_.Name -in $server.processes } | Select-Object -Property processid, name, workingsetsize, ThreadCount, HandleCount
+            # 1.檢查程式數量是否正確, 如果不對, 找出少那一個
+            if ($processes.count -ne $server.processes.count) {
+                $missingProcesses = Compare-Object -ReferenceObject $server.processes -DifferenceObject $processes.Name #-IncludeEqual -ExcludeDifferent 
+                Write-Host "Missing processes: $($missingProcesses.inputobject)" -ForegroundColor Red
+            }
+
+            # 連線到遠端電腦並取得指定程式的 CPU 使用率
+            Write-Debug "連線到 $($server.ip) 並取得指定程式的 CPU 使用率..."
+            $cpuUsage = Get-WmiObject -Class Win32_PerfFormattedData_PerfProc_Process -ComputerName $server.ip -Credential $credential |
             Where-Object { $_.IDProcess -in $processes.processid } 
 
-foreach ($process in $processes) {
-    # 在$cpuUsage中找到相同的processid
-    $cpuUsage_match = $cpuUsage | Where-Object { $_.IDProcess -eq $process.processid }
+            foreach ($process in $processes) {
+                write-debug "正在檢查 $($process.name)..."
+
+                # 在$cpuUsage中找到相同的processid
+                $cpuUsage_match = $cpuUsage | Where-Object { $_.IDProcess -eq $process.processid }
+        
+                # 如果找到, 就加入datatable
+                if ($cpuUsage_match -ne $null) {
+                    $datatable.Rows.Add($server.computername, $server.ip, (Get-Date), $process.name, $process.processid, $process.workingsetsize, $process.ThreadCount, $process.HandleCount, $cpuUsage_match.PercentProcessorTime) | Out-Null
+                }
+                else {
+                    # 如果沒找到, 就加入datatable, cpuUsage欄位填入'none'
+                    $datatable.Rows.Add($server.computername, $server.ip, (Get-Date), $process.name, $process.processid, $process.workingsetsize, $process.ThreadCount, $process.HandleCount, 'none') | Out-Null
+                }
     
-    # 如果找到, 就加入datatable
-    if ($cpuUsage_match -ne $null) {
-        $datatable.Rows.Add($server.computername, $server.ip, (Get-Date), $process.name, $process.processid, $process.workingsetsize, $process.ThreadCount, $process.HandleCount, $cpuUsage_match.PercentProcessorTime) | Out-Null
-    }
-    else {
-        # 如果沒找到, 就加入datatable, cpuUsage欄位填入'none'
-        $datatable.Rows.Add($server.computername, $server.ip, (Get-Date), $process.name, $process.processid, $process.workingsetsize, $process.ThreadCount, $process.HandleCount, 'none') | Out-Null
-    }
-
-    # 找出指定的程式, 並且按照resonseDateTime排序, 取出最新的2筆資料
-    $sortedtable = $datatable.Select( "processName = '$($process.name)'","resonseDateTime DESC")
-
-    # 找出最新2筆資料, 如果 workingsetsize, threadcount , handlecount 數值都一樣, 
-    # 表示程式可能當機了
-    $last2rows = $sortedtable | Select-Object -First 2
+                # 找出指定的程式, 並且按照resonseDateTime排序, 取出最新的2筆資料
+                $sortedtable = $datatable.Select( "processName = '$($process.name)'", "resonseDateTime DESC")
     
-    $last2rows | Format-Table -AutoSizes
+                # 2.找出最新2筆資料, 檢查如果 workingsetsize, threadcount , handlecount 數值都一樣, 
+                # 表示程式可能當機了
+                $last2rows = $sortedtable | Select-Object -First 2
+        
+                $last2rows | Format-Table -AutoSizes
+        
+                if (($last2rows.Count -eq 2) -and ($last2rows[0].processName -eq $last2rows[1].processName) -and ($last2rows[0].workingsetsize -eq $last2rows[1].workingsetsize) -and ($last2rows[0].ThreadCount -eq $last2rows[1].ThreadCount) -and ($last2rows[0].HandleCount -eq $last2rows[1].HandleCount)) {
+                    Write-Host "Warning: $($last2rows[0].processName) on $($last2rows[0].computername) may be crashed." -ForegroundColor Yellow
+                }
     
-    if (($last2rows.Count -eq 2) -and ($last2rows[0].processName -eq $last2rows[1].processName) -and ($last2rows[0].workingsetsize -eq $last2rows[1].workingsetsize) -and ($last2rows[0].ThreadCount -eq $last2rows[1].ThreadCount) -and ($last2rows[0].HandleCount -eq $last2rows[1].HandleCount)) {
-        Write-Host "Warning: $($last2rows[0].processName) on $($last2rows[0].computername) may be crashed." -ForegroundColor Yellow
+            }
+
+        }
+
+
     }
 
 
-
-
-}
-
-
-
-#$datatable | format-table 
-Start-Sleep -s 60
+    Start-Sleep -s 300
 }
 while (
     $true<# Condition that stops the loop if it returns false #>
