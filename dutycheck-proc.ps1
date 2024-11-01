93909addf8a0d617584fb0d9d89d45a2c822ae92
neoxlarge <neoxlarge@gmail.com>
2024-11-01 23:40:36 +0800

    整理程式

--- dutycheck-proc.ps1
+++
@@ -1,8 +0,0 @@
-$cred = Get-Credential
-$option = New-CimSessionOption -Protocol DCOM
-$session = New-CimSession -ComputerName 遠端主機IP或名稱 -Credential $cred -SessionOption $option
-
-Get-CimInstance -ClassName Win32_Process -Filter "Name like 'run1' or Name like 'run2' or Name like 'run3'" -CimSession $session | 
-Select-Object Name, ProcessId, @{N='CPU';E={$_.UserModeTime}}, @{N='Memory';E={$_.WorkingSetSize}}
-
-Remove-CimSession $session
